38b6d9fab143ce20689a12d80bb4d7382ba8ac62
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-10 10:50:54 +0000

    changed the dependency in Ingredient to :restrict_with_exception rather than with_error

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :doses
-  has_many :cocktails, through: :doses, dependent: :restrict_with_error
+  has_many :cocktails, through: :doses, dependent: :restrict_with_exception
 
   validates :name, uniqueness: true, presence: true
 end
