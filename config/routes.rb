69cefcba25bb653117f9472d838b1d654ca11377
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-10 17:14:50 +0000

    created UsersController and SessionsController for login/logout and signup actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
 
   get 'cocktails/:cocktail_id/doses', to: 'doses#new'
   resources :doses, only: [:destroy]
+
+  # Used for signing up a new user
+  resources :users, only: [:new, :create]
+
+  # Used for logging in and out
+  resources :sessions, only: [:new, :create, :destroy]
 end
