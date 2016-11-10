31bb43b87e34f59a22b89748bc3fa4846ace1d50
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-10 19:00:47 +0000

    fixed issue with routing on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:user][:email])
-    if @user && @user.authenticate(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
       session[:current_user_id] = @user.id
       redirect_to root_path
     else
