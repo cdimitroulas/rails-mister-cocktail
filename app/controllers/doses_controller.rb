f3564c3ca75310bbb7fafdb2f56badbe5b39c754
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-10 11:48:43 +0000

    created a doses controller with new/create/destroy actions and made the relevant views. The user is able to choose from a list of ingredients in a drop down menu and is able to delete the doses they have added to the cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+
+  before_action :set_cocktail
+
+  def new
+    @dose = @cocktail.doses.build
+    @ingredients_array = Ingredient.all.to_a
+  end
+
+  def create
+    @dose = @cocktail.doses.build(dose_params)
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
